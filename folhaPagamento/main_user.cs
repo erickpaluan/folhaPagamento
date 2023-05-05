804052cb9bcf5881d610f85cd3e3a228ca0f41c7
erickpaluan <epaluan@outlook.com>
2023-05-05 11:21:25 -0300

    inicio desenvolvimento lado usuário

--- folhaPagamento/main_user.cs
+++ folhaPagamento/main_user.cs
@@ -12,9 +12,33 @@ namespace folhaPagamento
 {
     public partial class main_user : Form
     {
-        public main_user()
+        public UserSession Session { get; set; }
+        public main_user(UserSession session)
         {
             InitializeComponent();
+            Session = session;
+        }
+
+        private void main_user_Load(object sender, EventArgs e)
+        {
+            string nome = Session.Username;
+            bool isAdm = Session.IsAdmin;
+
+            DateTime now = DateTime.Now;
+
+
+            if (now.Hour >= 5 && now.Hour < 12)
+            {
+                lblSaudacao.Text = "Bom dia, " + nome;
+            }
+            else if (now.Hour >= 12 && now.Hour < 18)
+            {
+                lblSaudacao.Text = "Boa tarde, " + nome;
+            }
+            else
+            {
+                lblSaudacao.Text = "Boa noite, " + nome;
+            }
         }
     }
 }
